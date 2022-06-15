35899f1d230fe23a69c046099c88c1a89cc69017
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-15 16:46:57 -0700

    doc strigs

--- app.py
+++ app.py
@@ -50,17 +50,30 @@ def add_user():
 
 @app.get('/users/<int:user_id>')
 def display_user_page(user_id):
-
+    """Show user page for desired user"""
     user = User.query.get_or_404(user_id)
     return render_template('detail.html', user=user)
 
 @app.get('/users/<int:user_id>/edit')
 def display_form_edit_user(user_id):
-
+    """Show edit form for the corresponding user"""
     user = User.query.get_or_404(user_id)
-
     return render_template('edit.html', user = user)
 
-# @app.post('/users/<int:user_id>/edit')
+@app.post('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    """Get info from edit user info form, update database, and redirect to users"""
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    img_url = request.form["img_url"]
+
+    user = User.query.get(user_id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.img_url = img_url
+
+    db.session.commit()
+
+    return redirect("/users")
 
 # @app.post('/users/<int:user_id>/delete')
